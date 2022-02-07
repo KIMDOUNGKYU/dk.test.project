acb39011dfb08f221a06e2106837655478b02423
KIMDOUNGKYU <sgrhrg@naver.com>
2022-02-07 16:12:11 +0900

    s

--- src/com/koreait/board/test.java
+++ src/com/koreait/board/test.java
@@ -3,7 +3,9 @@ package com.koreait.board;
 public class test {
     public static void main(String[] args) {
         String t1 = "test";
-
+        int t2= 2;
         System.out.println(t1);
+
+
     }
 }
